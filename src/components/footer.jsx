bc440def577eebff1bd063d2e89159cd7b32688d
ilarikoik <ilari.koik@gmail.com>
2024-12-18 19:41:47 +0200

    .

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,11 +2,14 @@ export default function Footer({ aos }) {
   return (
     <>
       <div
-        data-aos={aos}
-        className="bg-custom-beige text-olive h-32 flex flex-row justify-around items-center mt-2"
+        // data-aos={aos}
+        className="bg-custom-beige text-olive h-22 flex flex-row justify-around items-center mt-8"
       >
         <p>
           <strong>&copy; Ilari </strong>
+        </p>
+        <p>
+          <strong>Leetcode</strong>
         </p>
         <p>
           <strong>Github</strong>
